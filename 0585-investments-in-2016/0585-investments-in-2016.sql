12c2bcd9d3bc6caa72885c2751ce4a3463ea2fa7
Shivi Srivastava <shivisrivastava21@gmail.com>
2026-09-23 20:35:57 +0530

    Time: 745 ms (25.47%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,1 +1,1 @@
-SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016 FROM Insurance i WHERE (SELECT COUNT(*) FROM Insurance WHERE tiv_2015=i.tiv_2015)>1 AND (SELECT COUNT(*) FROM Insurance WHERE lat=i.lat AND lon=i.lon)=1;
+SELECT ROUND(SUM(tiv_2016),2) tiv_2016 FROM Insurance WHERE tiv_2015 IN (SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(*)>1) AND (lat,lon) IN (SELECT lat,lon FROM Insurance GROUP BY lat,lon HAVING COUNT(*)=1);
